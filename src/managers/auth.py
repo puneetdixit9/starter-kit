86c93e10ed0c43cc75ee4b35d9225d2669136608
Puneet Dixit <puneet.dixit@statusneo.com>
2023-04-12 12:56:37 +0530

    Desc: pre-commit hooks added

--- src/managers/auth.py
+++ src/managers/auth.py
@@ -1,17 +1,18 @@
-import settings
-from flask import jsonify
+import enum
 
-from src.database.models.auth import User, TokenBlocklist
+from flask import jsonify
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
+    get_jwt,
     get_jwt_identity,
-    get_jwt
 )
+from werkzeug.security import check_password_hash, generate_password_hash
+
+import settings
 from src.database import db
+from src.database.models.auth import TokenBlocklist, User
 from src.utils import update_class_object
-from werkzeug.security import generate_password_hash, check_password_hash
-import enum
 
 
 class ROLE(enum.Enum):
@@ -35,12 +36,16 @@ class AuthManager:
     def create_new_user(cls, user_data):
         error_data = {}
         user_by_email = User.query.filter_by(email=user_data["email"]).first()
-        user_by_username = User.query.filter_by(username=user_data["username"]).first()
+        user_by_username = User.query.filter_by(
+            username=user_data["username"]
+        ).first()
         if user_by_email or user_by_username:
             param = "username" if user_by_username else "email"
             error_data["error"] = f"user already exists with provided {param}"
         else:
-            user_data["password"] = generate_password_hash(user_data["password"])
+            user_data["password"] = generate_password_hash(
+                user_data["password"]
+            )
             user = User(**user_data)
             db.session.add(user)
             db.session.commit()
@@ -57,32 +62,49 @@ class AuthManager:
     @classmethod
     def update_user_password(cls, update_password_data={}):
         user = cls.get_current_user()
-        if check_password_hash(user.password, update_password_data['old_password']):
-            if check_password_hash(user.password, update_password_data['new_password']):
-                return jsonify(error="new password can not same as old password"), 403
-            user.password = generate_password_hash(update_password_data['new_password'])
+        if check_password_hash(
+            user.password, update_password_data["old_password"]
+        ):
+            if check_password_hash(
+                user.password, update_password_data["new_password"]
+            ):
+                return (
+                    jsonify(error="new password can not same as old password"),
+                    403,
+                )
+            user.password = generate_password_hash(
+                update_password_data["new_password"]
+            )
             db.session.commit()
             return jsonify({"status": "success"}), 200
-        return jsonify(error='Old password is invalid'), 403
+        return jsonify(error="Old password is invalid"), 403
 
     @classmethod
     def get_token(cls, login_data):
-        email_or_username = login_data.get("username") or login_data.get("email")
-        user = db.session.query(User).filter(
-            (User.email == email_or_username) | (User.username == email_or_username)).first()
+        email_or_username = login_data.get("username") or login_data.get(
+            "email"
+        )
+        user = (
+            db.session.query(User)
+            .filter(
+                (User.email == email_or_username)
+                | (User.username == email_or_username)
+            )
+            .first()
+        )
         if not user:
             return {"error": f"user not found with {email_or_username}"}, 403
 
-        if check_password_hash(user.password, login_data['password']):
+        if check_password_hash(user.password, login_data["password"]):
             access_token = create_access_token(identity={"user_id": user.id})
             refresh_token = create_refresh_token(identity={"user_id": user.id})
             return {
                 "access_token": access_token,
                 "refresh_token": refresh_token,
-                "expire_in": settings.TOKEN_EXPIRE_IN * 60
+                "expire_in": settings.TOKEN_EXPIRE_IN * 60,
             }, 200
         return {"error": "wrong password"}, 403
-    
+
     @classmethod
     def logout(cls):
         token = get_jwt()
@@ -103,4 +125,6 @@ class AuthManager:
     def refresh_access_token(cls):
         identity = get_jwt_identity()
         access_token = create_access_token(identity=identity)
-        return jsonify(access_token=access_token, expire_in=60 * settings.TOKEN_EXPIRE_IN)
+        return jsonify(
+            access_token=access_token, expire_in=60 * settings.TOKEN_EXPIRE_IN
+        )
--- src/managers/jwt.py
+++ src/managers/jwt.py
@@ -1,4 +1,5 @@
 from flask_jwt_extended import JWTManager
+
 from src.managers import AuthManager
 
 jwt = JWTManager()
