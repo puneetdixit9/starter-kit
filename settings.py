04578fd44be370e447befe3d6daa7dcc5a0db24f
Puneet Dixit <puneet.dixit@statusneo.com>
2023-04-13 15:48:15 +0530

    Desc: logs base dir created

--- settings.py
+++ settings.py
@@ -8,9 +8,6 @@ dotenv_path = os.path.join(os.path.expanduser("~"), "Desktop", "starter_app.env"
 load_dotenv(dotenv_path)
 
 
-if not os.path.exists("logs"):
-    os.makedirs("logs")
-
 SECRET_KEY = "test"
 TOKEN_EXPIRE_IN = 360
 
@@ -27,6 +24,8 @@ TEST_CONFIG["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABAS
 
 
 LOGS_BASE_DIR = "logs"
+if not os.path.exists(LOGS_BASE_DIR):
+    os.makedirs(LOGS_BASE_DIR)
 ERROR = logging.ERROR
 INFO = logging.INFO
 WARNING = logging.WARNING
