0ae3dc1eb6b8fe0deb08daa0547bb332889c744e
Puneet Dixit <puneet.dixit@statusneo.com>
2023-04-13 15:44:50 +0530

    Desc: .env file and load_dotenv added

--- settings.py
+++ settings.py
@@ -1,32 +1,30 @@
+import logging
 import os
+from datetime import timedelta
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.expanduser("~"), "Desktop", "starter_app.env")
+load_dotenv(dotenv_path)
+
 
 if not os.path.exists("logs"):
     os.makedirs("logs")
 
-import logging
-from datetime import timedelta
-
 SECRET_KEY = "test"
 TOKEN_EXPIRE_IN = 360
 
-
 CONFIG = {
-    "SECRET_KEY": SECRET_KEY,
-    "SQLALCHEMY_DATABASE_URI": "sqlite:///db.sqlite",
-    "JWT_SECRET_KEY": "secret",
-    "JWT_ACCESS_TOKEN_EXPIRES": timedelta(minutes=TOKEN_EXPIRE_IN),
-    "JWT_REFRESH_TOKEN_EXPIRES": timedelta(days=1),
-    "TESTING": False,
+    "SECRET_KEY": os.environ.get("SECRET_KEY"),
+    "SQLALCHEMY_DATABASE_URI": os.environ.get("SQLALCHEMY_DATABASE_URI"),
+    "JWT_SECRET_KEY": os.environ.get("JWT_SECRET_KEY"),
+    "JWT_ACCESS_TOKEN_EXPIRES": timedelta(minutes=int(os.environ.get("JWT_ACCESS_TOKEN_EXPIRES_TIME"))),
+    "JWT_REFRESH_TOKEN_EXPIRES": timedelta(minutes=int(os.environ.get("JWT_REFRESH_TOKEN_EXPIRES_TIME"))),
 }
 
-TEST_CONFIG = {
-    "SECRET_KEY": SECRET_KEY,
-    "SQLALCHEMY_DATABASE_URI": "sqlite:///test_db.sqlite",
-    "JWT_SECRET_KEY": "secret",
-    "JWT_ACCESS_TOKEN_EXPIRES": timedelta(minutes=TOKEN_EXPIRE_IN),
-    "JWT_REFRESH_TOKEN_EXPIRES": timedelta(days=1),
-    "TESTING": True,
-}
+TEST_CONFIG = CONFIG.copy()
+TEST_CONFIG["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+
 
 LOGS_BASE_DIR = "logs"
 ERROR = logging.ERROR
